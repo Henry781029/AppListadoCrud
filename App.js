1970e37dac047f2acaabb6576d46d5a1ed84dff6
Henry781029 <hcramosp@outlook.com>
2022-06-11 13:11:04 -0500

    arreglos

--- App.js
+++ App.js
@@ -5,11 +5,38 @@ import {
   Button, 
   Text } from 'react-native'
 import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
-  return (
-    <View>
-      <Text>MyCrudApp</Text>
+const Stack = createNativeStackNavigator();
+
+function MisPantallas(){
+  return(
+    <View style ={estilos.vistas}>
+      <Text>
+        Crear listas
+      </Text>
     </View>
   )
 }
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name='LISTADO'
+          component={MisPantallas}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+const estilos = StyleSheet.create({
+  vistas:{
+    flex: 1,
+    alignItems:"center",
+    justifyContent:"center"
+  }
+})
