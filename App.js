821140505fd919826007fc6ddff0e4f7e1106902
Henry781029 <hcramosp@outlook.com>
2022-06-11 11:14:58 -0500

    e crea proyecto Appcrud

---
+++ App.js
@@ -0,0 +1,15 @@
+import { 
+  View,
+  StyleSheet,
+  TextInput,
+  Button, 
+  Text } from 'react-native'
+import React from 'react'
+
+export default function App() {
+  return (
+    <View>
+      <Text>MyCrudApp</Text>
+    </View>
+  )
+}
